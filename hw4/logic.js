04858ba71d4005bce1ce5d429ea34600b3a1d932
r4ty34r <83795023+r4ty34r@users.noreply.github.com>
2022-04-06 13:30:57 -0400

    Update logic.js

--- hw4/logic.js
+++ hw4/logic.js
@@ -1,43 +1,20 @@
+var input = document.getElementById('inputBox');
+var button = document.getElementById('btn')
+var p1 = document.getElementById('p1');
+var p2 = document.getElementById('p2');
 
-document.getElementById("btn").onclick = compute;
-document.getElementById("newbut").onclick =newcompute;
+var bottombutton = document.getElementById('bottombutton');
 
-function compute() {
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-var input3 = document.getElementById("num3");
-var input4 = document.getElementById("num4");
-var answer2 = document.getElementById("answer");
-var result = " ";
-result= input2.value + input3.value + input4.value;
-oddstr = 4;
-var finstr = result + oddstr;
-
-answer2.innerHTML = finstr;
-
-result1 = parseInt(input1.value) +parseInt(input2.value) +parseInt(input3.value);
-input4.value = result1;
-
+function clearTextBox(){
+    input.innerHTML= " ";
 }
 
-function newcompute() {
-
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-var input3 = document.getElementById("num3");
-var input4 = document.getElementById("num4");
-
-result1 = parseInt(input1.value) +parseInt(input2.value) +parseInt(input3.value)+ parseInt(input4.value);
-
-objs=document.getElementById("p1");
-objt=document.getElementById("ha");
-
-objs.innerHTML = result1;
-objt.innerHTML= result1;
-input1.value = objs.innerHTML;
-
-
-
+function thing(){
+    x = input.textContent;
+    p1.innerHTML = x;
+    p2.innerHTML = x;
 }
 
+button.addEventListener('click', thing);
+bottombutton.addEventListener('click',clearTextBox);
 
