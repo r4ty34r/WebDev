e3983cff7d403c20e9bc9a9146f84656586b82c0
r4ty34r <83795023+r4ty34r@users.noreply.github.com>
2022-04-07 12:56:27 -0400

    Update logic.js

--- hw4/logic.js
+++ hw4/logic.js
@@ -2,19 +2,26 @@ var input = document.getElementById('inputBox');
 var button = document.getElementById('btn')
 var p1 = document.getElementById('p1');
 var p2 = document.getElementById('p2');
-
 var bottombutton = document.getElementById('bottombutton');
 
-function clearTextBox(){
-    input.innerHTML= " ";
-}
 
-function thing(){
-    x = input.textContent;
+console.log(input.value);
+
+function myFunction (){ 
+    const x = input.value;
     p1.innerHTML = x;
     p2.innerHTML = x;
 }
 
-button.addEventListener('click', thing);
-bottombutton.addEventListener('click',clearTextBox);
 
+function clear(){
+    const y = null;
+    p1.innerHTML = y;
+    p2.innerHTML = y;
+}
+
+
+function replaceText(){
+    const x = "Replace the text";
+    input.innerHTML = x;
+}
